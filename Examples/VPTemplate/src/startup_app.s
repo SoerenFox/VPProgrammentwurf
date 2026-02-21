6aaeba0fd133b93634541de939286782bc076d9e
SoerenFox <soeren.0211@gmail.com>
2026-02-21 15:24:54 -0500

    Fill stack and move vector table because of signature segment

--- Examples/VPTemplate/src/startup_app.s
+++ Examples/VPTemplate/src/startup_app.s
@@ -12,8 +12,8 @@
  *
  *
  *****************************************************************************/
-
-.syntax unified
+ 
+ .syntax unified
 .cpu cortex-m4
 .fpu softvfp
 .thumb
@@ -28,16 +28,31 @@
  *
  * @retval : None
  */
+
+/* SCB VTOR register address (Cortex-M4) */
+.equ SCB_VTOR, 0xE000ED08
+
 .section .text.StartApp_Handler
 .type StartApp_Handler, %function
 .global StartApp_Handler
 StartApp_Handler:
+    /* Relocate Vector Table because .signature is before .isr_vector */
+    ldr r0, =_app_vector_table      /* computed constant below */
+    ldr r1, =SCB_VTOR
+    str r0, [r1]
+
+    dsb
+    isb
+
     /* Copy the data segment initializers from flash to SRAM */
     ldr r0, =_sdata
     ldr r1, =_edata
     ldr r2, =_sloaddata
     movs r3, #0
     b .loopCopyData
+
+MAGIC_STACK_VALUE:
+	.word 0xDEADBEEF
 
 .copyData:
     ldr r4, [r2, r3]
@@ -63,6 +78,21 @@ StartApp_Handler:
     cmp r2, r4
     bcc .fillZerobss
 
+   ldr r0, = _bottom_of_stack
+   ldr r1, = _top_of_stack
+   ldr r2, = MAGIC_STACK_VALUE
+   ldr r2, [r2]
+   b .loopFillStack
+
+.fillStack:
+   str  r2, [r0]
+   adds r0, r0, #4
+
+.loopFillStack:
+   cmp r0, r1
+   bne .fillStack    
+   bcc .fillStack    
+
     /* Initialize the Stack-Pointer */
     /* Load address of initial_stack_pointer into R0 for. Symbol defined in Linker Script */
     ldr r0, =_initial_stack_pointer
@@ -76,3 +106,14 @@ StartApp_Handler:
     bl main
     bx lr
 .size StartApp_Handler, .-StartApp_Handler
+
+
+/* ---- Vector table address constant ----
+   This MUST match your linker layout:
+   FLASH origin = 0x08010000
+   .signature   = 4 bytes
+   => .isr_vector begins at 0x08010004
+
+   If signature size changes, update this or (better) export a linker symbol for it.
+*/
+_app_vector_table = 0x08010200
