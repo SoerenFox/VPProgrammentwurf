cf86cda6bc76c84471d5adc8c8872537f6ccb78e
Sören Fox <soeren.0211@gmail.com>
2026-02-23 04:39:58 -0500

    Add explanation for vector offset in app startup

--- Examples/VPTemplate/src/startup_app.s
+++ Examples/VPTemplate/src/startup_app.s
@@ -108,12 +108,5 @@ MAGIC_STACK_VALUE:
 .size StartApp_Handler, .-StartApp_Handler
 
 
-/* ---- Vector table address constant ----
-   This MUST match your linker layout:
-   FLASH origin = 0x08010000
-   .signature   = 4 bytes
-   => .isr_vector begins at 0x08010004
-
-   If signature size changes, update this or (better) export a linker symbol for it.
-*/
+/* .isr_vector offset -> Signature -> but Bit [0:8] must be 0 –> 1 0000 0000 = 0x200 in Hex */
 _app_vector_table = 0x08010200
