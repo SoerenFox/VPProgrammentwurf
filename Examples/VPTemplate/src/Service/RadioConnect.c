f0c1a983e0dfceb9851a656365c95c42d17db088
SoerenFox <soeren.0211@gmail.com>
2026-02-27 08:19:54 -0500

    Add checksum calculation to Radio Connect Module

--- Examples/VPTemplate/src/Service/RadioConnect.c
+++ Examples/VPTemplate/src/Service/RadioConnect.c
@@ -7,3 +7,16 @@ int32_t radioConnectInitialize(RadioConnect* pRadioConnect) {
 
 	return CONNECT_OK;
 }
+
+int32_t radioConnectSetChecksum(RadioConnect* pRadioConnect) {
+	if (!pRadioConnect) return CONNECT_INVALID_PTR;
+
+	int sum = 0;
+	for (int i = 0; i < CONNECT_STRUCT_SIZE; i++) {
+		sum += *(pRadioConnect >> i);
+	}
+	int lsb = (uint8_t)sum;
+	pRadioConnect->checksum = (uint8_t)(lsb ^ 0xFF + 0x01); // TODO: get rid of magic numbers
+
+	return CONNECT_OK;
+}
--- Examples/VPTemplate/src/Service/RadioConnect.h
+++ Examples/VPTemplate/src/Service/RadioConnect.h
@@ -7,15 +7,18 @@
 #define CONNECT_INVALID_PTR -1
 #define CONNECT_VALUE_INVALID -2
 
+#define CONNECT_STRUCT_SIZE 7 // Structure size without checksum in bytes
+
 #pragma pack(push, 1)
 typedef struct _RadioConnect {
 	uint8_t packetCounter;
 	uint32_t sensorValue;
 	uint16_t reserved;
-	int8_t chechsum;
+	int8_t checksum;
 } RadioConnect;
 #pragma pack(pop)
 
 int32_t radioConnectInitialize(RadioConnect* pRadioConnect);
+int32_t radioConnectSetChecksum(RadioConnect* pRadioConnect);
 
 #endif
