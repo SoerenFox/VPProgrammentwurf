453ea8cd23065ae6d4cda25cdae7d4d91e13b469
Sören Fox <soeren.0211@gmail.com>
2026-02-28 06:47:41 -0500

    Fix pointer syntax in checksum calculation of Radio Connect Module

--- Examples/VPTemplate/src/Service/RadioConnect.c
+++ Examples/VPTemplate/src/Service/RadioConnect.c
@@ -11,12 +11,14 @@ int32_t radioConnectInitialize(RadioConnect* pRadioConnect) {
 int32_t radioConnectSetChecksum(RadioConnect* pRadioConnect) {
 	if (!pRadioConnect) return CONNECT_INVALID_PTR;
 
+	uint8_t* bytes = (uint8_t*)pRadioConnect;
+
 	int sum = 0;
 	for (int i = 0; i < CONNECT_STRUCT_SIZE; i++) {
-		sum += *(pRadioConnect >> i);
+		 sum += bytes[i];
 	}
 	int lsb = (uint8_t)sum;
-	pRadioConnect->checksum = (uint8_t)(lsb ^ 0xFF + 0x01); // TODO: get rid of magic numbers
+	pRadioConnect->checksum = (uint8_t)((lsb ^ 0xFF) + 0x01); // TODO: get rid of magic numbers
 
 	return CONNECT_OK;
 }
