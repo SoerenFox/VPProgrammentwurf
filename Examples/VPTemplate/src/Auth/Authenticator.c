1f521696469ba1bc29e7cbbf1554890d4d2cf5ce
Sören Fox <soeren.0211@gmail.com>
2026-03-03 06:39:27 -0500

    Fix verify function and add function that copies .auth to RAM

--- Examples/VPTemplate/src/Auth/Authenticator.c
+++ Examples/VPTemplate/src/Auth/Authenticator.c
@@ -2,16 +2,29 @@
 
 #include "stm32g4xx.h"
 
-void verify(void) {
+/**
+ * @brief Copies .auth section from Flash to RAM.
+ */
+void copyAuthToRam(void) {
+	uint8_t* src = &_sloadauth;
+	uint8_t* dst = &_sauth;
 
+	while (dst < &_eauth)
+		*dst++ = *src++;
+}
+
+/**
+ * @brief Checks signature bytes and if correct, it calls the StartHandler() function of the Application.
+ */
+void verify(void) {
 	const char* sig = (const char*)APP_SIGNATURE_ADDR;
 
 	if (sig[0] == 'U' && sig[1] == 'M' && sig[2] == 'M' && sig[3] == 'S') {
 		__disable_irq();
 
-	    uint32_t *pStartApp = (uint32_t *)APP_HANDLER_ADDR;
-	    StartHandler_t start = (StartHandler_t) *(pStartApp);
-	    start();
+		StartHandler_t startApp = (StartHandler_t)*(uint32_t*)(APP_HANDLER_ADDR);
+
+		startApp();
 	}
 	while(1) {}
 }
--- Examples/VPTemplate/src/Auth/Authenticator.h
+++ Examples/VPTemplate/src/Auth/Authenticator.h
@@ -4,10 +4,15 @@
 #include <stdint.h>
 
 #define APP_SIGNATURE_ADDR 0x08010000 // address of signature segment
-#define APP_HANDLER_ADDR 0x08010204 // vector table adress + 4 for handler
+#define APP_HANDLER_ADDR 0x08010204 // vector table address + 4 for handler (its the second word in the vector table right after the initial stack pointer)
 
 typedef void (*StartHandler_t)(void);
 
+extern uint8_t _sloadauth;
+extern uint8_t _sauth;
+extern uint8_t _eauth;
+
+void copyAuthToRam(void);
 void verify(void) __attribute__ ((section (".auth")));
 
 #endif
